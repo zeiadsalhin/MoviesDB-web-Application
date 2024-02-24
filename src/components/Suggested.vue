c6d3af90e026c62dbcbb2c421573944df49dc752
zeiadsalhin <zeyadali011@gmail.com>
2024-02-24 19:26:19 +0200

    v1

--- src/components/Suggested.vue
+++ src/components/Suggested.vue
@@ -116,7 +116,7 @@ export default {
     },
     methods: {
         async fetchMovies() {
-            const movieId = this.$route.params.id;
+            const movieId2 = this.$route.params.id;
             const options = {
                 method: 'GET',
                 headers: {
@@ -126,7 +126,7 @@ export default {
             };
 
             try {
-                const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/similar?language=en-US&page=1`, options);
+                const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId2}/similar?language=en-US&page=1`, options);
                 const data = await response.json();
                 this.movies = [...this.movies, ...data.results];
                 this.totalPages = data.total_pages;
