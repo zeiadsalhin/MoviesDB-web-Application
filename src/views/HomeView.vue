8e2d0e49767781f130657d9fa0afbbea40cfe7aa
zeiadsalhin <zeyadali011@gmail.com>
2024-02-25 20:55:32 +0200

    v1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,38 @@ import latest2 from "../components/LatestTV.vue"
 
 <template>
   <main>
-    <banner />
-    <latest />
-    <latest2 />
+    <div v-if="isloading" class=""
+      style="height: 100vh;width: 100%; background-color: black; position: absolute; z-index: 999;">
+      <div class="load m-auto" style="position: relative;top: 50%;transform: translateX(50%);overflow: hidden;">
+        <v-progress-circular indeterminate></v-progress-circular>
+      </div>
+    </div>
+    <transition name="fade" mode="out-in">
+      <banner />
+    </transition>
+    <transition name="fade" mode="out-in">
+      <latest />
+    </transition>
+    <transition name="fade" mode="out-in">
+      <latest2 />
+    </transition>
   </main>
 </template>
+<style>
+html,
+body {
+  overflow: hidden;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      isloading: true,
+    }
+  },
+  mounted() {
+    this.isloading = false
+  },
+}
+</script>
