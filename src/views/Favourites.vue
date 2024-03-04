1df44acd0857794a6b5ad300c73434bb1930ed6a
zeiadsalhin <zeyadali011@gmail.com>
2024-03-04 15:25:10 +0200

    v1

--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -50,42 +50,7 @@
             </div>
             <!-- </v-lazy> -->
         </div>
-        <div class="2">
-            <div class="cast md:flex-col md:p-10 p-1 md:space-x-10 bg-zinc-950 mt-5 w-full">
-                <div class="text text-2xl font-medium p-5">
-                    <h1>Top People this Month</h1>
-                </div>
-                <div v-if="movieCredits != null" class="scroll-container" ref="scrollContainer"
-                    style="height: fit-content;">
-                    <button class="scroll-button left" @click="scrollLeft" v-show="scrollLeftButtonVisible"
-                        v-if="!$vuetify.display.mobile">
-                        <v-icon icon="mdi-chevron-left"></v-icon>
-                    </button>
-                    <div class="movie-list" ref="movieList">
-                        <div v-for="person in movieCredits" :key="person.id" class="person p-2">
-                            <div class="imagecover"
-                                style=" width: 8rem;height: 100%;margin: 0.1rem;height: fit-content;"><v-img
-                                    v-if="person.profile_path"
-                                    :src="'https://image.tmdb.org/t/p/w154' + person.profile_path" alt="Person"
-                                    width="100%" height="100%"
-                                    class="poster mx-auto hover:scale-105 transform transition ease-in-out duration-300"></v-img>
-                                <v-img v-else src="/error.svg"></v-img>
-                            </div>
-                            <h3 class="font-semibold md:text-md text-left py-2">{{ person.name }}</h3>
-                            <p class="opacity-70 text-sm text-left">{{ person.character }}</p>
 
-                        </div>
-                    </div>
-                    <button class="scroll-button right" @click="scrollRight" v-show="scrollRightButtonVisible"
-                        v-if="!$vuetify.display.mobile && movieCredits.length > 8">
-                        <v-icon icon="mdi-chevron-right"></v-icon>
-                    </button>
-                </div>
-                <div v-else class="flex justify-center m-auto p-5">
-                    <div class="load m-auto"><v-progress-circular indeterminate></v-progress-circular></div>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
@@ -154,7 +119,6 @@ export default {
             loading: true,
             movies: [],
             visibleMovies: [],
-            movieCredits: null,
             currentPage: 1,
             pageSize: 20,
             totalPages: null,
@@ -164,7 +128,6 @@ export default {
     },
     mounted() {
         this.fetchMovies();
-        this.fetchMovieDetails();
     },
     methods: {
         async fetchMovies() {
@@ -178,30 +141,9 @@ export default {
 
             try {
                 const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${this.currentPage}`, options);
-                const data1 = await response.json();
-                this.movies = [...this.movies, ...data1.results];
-                this.totalPages = data1.total_pages;
-                this.loadVisibleMovies();
-                this.loading = false
-            } catch (error) {
-                console.error(error);
-            }
-        },
-        async fetchMovieDetails() {
-            try {
-                // const movieId = this.$route.params.id;
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        accept: 'application/json',
-                        Authorization: import.meta.env.VITE_API_KEY
-                    }
-                };
-                const url = `https://api.themoviedb.org/3/person/popular?language=en-US&page=1`;
-
-                const response = await fetch(url, options);
-                const data2 = await response.json();
-                this.movieCredits = data2;
+                const data = await response.json();
+                this.movies = [...this.movies, ...data.results];
+                this.totalPages = data.total_pages;
                 this.loadVisibleMovies();
                 this.loading = false
             } catch (error) {
